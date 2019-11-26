e881e70da739665a4fcd5dec41dc2e3d46ff826f
Attila <attila@p4d.co.uk>
2019-11-26 12:55:56 +0000

    displaying all jobs ready & paginating 2 per page is ready
    needs more styling off course

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Jobs;
 
 class HomeController extends Controller
 {
-    //
+    public function index(Request $Request) {
+
+
+    	// get all available jobs
+    	$jobs = Jobs::paginate(2);    
+//var_dump($jobs);exit;
+    	
+    	if (count($jobs) > 0) {
+    		return view('welcome', compact('jobs'));
+    	}
+
+    	//fallback
+    	$jobs = array();
+    	return view('welcome', compact('jobs'));
+    	
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,10 +21,38 @@
     </div>
     <div class="col-md-9" style="background-color: green">
         <div class="col-md-12">
-            reults total 10...
+           @if(count($jobs) > 0)
+                Total jobs {{count($jobs)}}
+           @endif
         </div>
 
-        
+        @foreach($jobs as $job)
+            <div class="col-md-12 job-container mb-3">
+                
+                <!-- Title -->
+                <h4><strong>{{$job->job_title}}</strong></h4>
+
+                <!-- details -->
+                <div>
+                    <span><i class="fa fa-map-marker-alt"></i> {{$job->job_location}}</span> 
+                    <span><i class="fa fa-pound-sign"></i> {{$job->job_salary}}</span>
+                </div>
+
+                <!-- description -->
+                <p>
+                    {{$job->job_description}}
+                </p>
+
+                <!-- action buttons -->
+                <button class="btn btn-sm">MORE DETAILS</button>
+                <button class="btn btn-sm">APPLY</button>
+
+            </div>
+        @endforeach
+
+        {{ $jobs->links() }}
+
+
     </div>
 </div>
 
