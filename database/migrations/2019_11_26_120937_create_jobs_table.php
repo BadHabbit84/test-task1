e4f794c40d40fb27d7893041118f98cfc487ea1a
Attila <attila@p4d.co.uk>
2019-11-26 12:14:49 +0000

    migration file for jobs table

---
+++ database/migrations/2019_11_26_120937_create_jobs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('job_title', 100)->default(null);
+            $table->string('job_description', 200)->default(null);
+            $table->string('job_location', 100)->default(null);
+            $table->integer('job_salary',7)->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
